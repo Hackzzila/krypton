cd32618d36e2722eec9deec5227ef8f8d79209b9
Hackzzila <admin@hackzzila.com>
2018-04-06 00:00:36 -0500

    windows builds

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const { get, post } = require('superagent');
 const tar = require('tar');
+const xml = require('xml-js');
 
 const arches = {
   win32: ['x64', 'ia32'],
@@ -95,7 +96,7 @@ async function compileLibrary(name, arch, url) {
 if (!fs.existsSync('deps')) fs.mkdirSync('deps');
 if (!fs.existsSync('builds')) fs.mkdirSync('builds');
 
-(async () => {
+async function linux() {
   for (const arch of arches[process.platform]) {
     if (!fs.existsSync(path.resolve('deps', arch))) fs.mkdirSync(path.resolve('deps', arch));
     if (!fs.existsSync(path.resolve('deps', arch, 'install'))) fs.mkdirSync(path.resolve('deps', arch, 'install'));
@@ -128,4 +129,70 @@ if (!fs.existsSync('builds')) fs.mkdirSync('builds');
       fs.copyFileSync(path.resolve('build', 'Release', 'krypton.node'), path.resolve('builds', `${process.platform}-${arch}-${id}.node`));
     }
   }
-})();
+}
+
+async function windows() {
+  for (const arch of arches[process.platform]) {
+    const config = {
+      target_defaults: {
+        defines: ['KRYPTON_DISABLE_LAME', 'SODIUM_STATIC'],
+      },
+    };
+
+    if (arch === 'ia32') config.target_defaults.defines.push('KRYPTON_DISABLE_SODIUM');
+
+    fs.writeFileSync('config.gypi', JSON.stringify(config, undefined, 2));
+
+    for (const target of targets) {
+      const id = abi.getAbi(target, 'node');
+
+      execSync(`${path.resolve('node_modules', '.bin', 'node-gyp')} configure --target=v${target} --arch=${arch}`, {
+        stdio: 'inherit',
+        env: {
+          ...process.env,
+          ...env[arch],
+        },
+      });
+
+      const vcxproj = xml.xml2js(fs.readFileSync(path.resolve('build', 'krypton.vcxproj'), 'utf8'));
+
+      const globals = vcxproj.elements.find(x => x.name === 'Project').elements.find(x => x.name === 'PropertyGroup' && x.attributes.Label === 'Globals');
+
+      globals.elements.push({
+        type: 'element',
+        name: 'VcpkgTriplet',
+        elements: [{
+          type: 'text',
+          text: `${arch === 'x64' ? 'x64' : 'x86'}-windows-static`,
+        }],
+      });
+
+      globals.elements.push({
+        type: 'element',
+        name: 'VcpkgEnabled',
+        elements: [{
+          type: 'text',
+          text: 'true',
+        }],
+      });
+
+      fs.writeFileSync(path.resolve('build', 'krypton.vcxproj'), xml.js2xml(vcxproj, { spaces: 2 }));
+
+      execSync(`${path.resolve('node_modules', '.bin', 'node-gyp')} build --target=v${target} --arch=${arch}`, {
+        stdio: 'inherit',
+        env: {
+          ...process.env,
+          ...env[arch],
+        },
+      });
+
+      fs.copyFileSync(path.resolve('build', 'Release', 'krypton.node'), path.resolve('builds', `${process.platform}-${arch}-${id}.node`));
+    }
+  }
+}
+
+if (process.platform === 'win32') {
+  windows();
+} else {
+  linux();
+}
